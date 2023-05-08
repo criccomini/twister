83e38a3009f9c60da6ede405d9226361daa44b0e
Chris Riccomini <criccomini@apache.org>
2023-05-08 09:24:36 -0700

    Add BytesToPojo

---
+++ twister-proto/src/main/java/dev/twister/proto/BytesToPojo.java
@@ -0,0 +1,176 @@
+package dev.twister.proto;
+
+import com.google.protobuf.Descriptors;
+import com.google.protobuf.Descriptors.Descriptor;
+import com.google.protobuf.Descriptors.FieldDescriptor;
+import com.google.protobuf.Descriptors.OneofDescriptor;
+
+import java.nio.ByteBuffer;
+import java.nio.ByteOrder;
+import java.nio.charset.StandardCharsets;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class BytesToPojo {
+
+    public static Map<String, Object> read(ByteBuffer byteBuffer, Descriptor descriptor) {
+        Map<String, Object> resultMap = new HashMap<>();
+
+        // Initialize resultMap with default values
+        for (FieldDescriptor fieldDescriptor : descriptor.getFields()) {
+            if (fieldDescriptor.hasDefaultValue()) {
+                String fieldName = fieldDescriptor.getName();
+                Object defaultValue = fieldDescriptor.getDefaultValue();
+                resultMap.put(fieldName, defaultValue);
+            }
+        }
+
+        while (byteBuffer.hasRemaining()) {
+            int key = (int) readVarint(byteBuffer);
+            int wireType = key & 0x07;
+            int fieldNumber = key >>> 3;
+
+            FieldDescriptor fieldDescriptor = descriptor.findFieldByNumber(fieldNumber);
+
+            if (fieldDescriptor == null) {
+                throw new IllegalArgumentException("Unknown field number: " + fieldNumber);
+            }
+
+            OneofDescriptor oneofDescriptor = fieldDescriptor.getContainingOneof();
+            boolean isOneof = oneofDescriptor != null;
+
+            String fieldName = fieldDescriptor.getName();
+
+            switch (wireType) {
+                case 0: // Varint, SignedVarint, Bool, Enum
+                    long rawValue = readVarint(byteBuffer);
+                    Object value;
+
+                    switch (fieldDescriptor.getType()) {
+                        case BOOL:
+                            value = rawValue != 0;
+                            break;
+                        case INT32:
+                            value = (int) rawValue;
+                            break;
+                        case SINT32:
+                            value = decodeZigZag32((int) rawValue);
+                            break;
+                        case SINT64:
+                            value = decodeZigZag64(rawValue);
+                            break;
+                        case ENUM:
+                            Descriptors.EnumValueDescriptor enumValueDescriptor = fieldDescriptor.getEnumType().findValueByNumber((int) rawValue);
+                            if (enumValueDescriptor == null) {
+                                throw new IllegalArgumentException("Unknown enum value: " + rawValue + " for field number: " + fieldNumber);
+                            }
+                            value = enumValueDescriptor.getName();
+                            break;
+                        default:
+                            value = rawValue;
+                            break;
+                    }
+
+                    addToResultMap(resultMap, fieldName, value, fieldDescriptor.isRepeated(), isOneof);
+                    break;
+                case 1: // Fixed64, SFixed64, Double
+                    long rawFixed64Value = byteBuffer.order(ByteOrder.LITTLE_ENDIAN).getLong();
+                    if (fieldDescriptor.getType() == FieldDescriptor.Type.DOUBLE) {
+                        addToResultMap(resultMap, fieldName, Double.longBitsToDouble(rawFixed64Value), fieldDescriptor.isRepeated(), isOneof);
+                    } else {
+                        // Fixed64 and SFixed64 are both encoded as 64-bit integers
+                        addToResultMap(resultMap, fieldName, rawFixed64Value, fieldDescriptor.isRepeated(), isOneof);
+                    }
+                    break;
+                case 2: // Length-Delimited
+                    // Check if the field is a map field
+                    if (fieldDescriptor.getType() == FieldDescriptor.Type.MESSAGE && fieldDescriptor.getMessageType().getOptions().getMapEntry()) {
+                        Descriptor mapEntryDescriptor = fieldDescriptor.getMessageType();
+                        FieldDescriptor keyField = mapEntryDescriptor.findFieldByNumber(1);
+                        FieldDescriptor valueField = mapEntryDescriptor.findFieldByNumber(2);
+
+                        int length = (int) readVarint(byteBuffer);
+                        byte[] bytes = new byte[length];
+                        byteBuffer.get(bytes);
+                        ByteBuffer nestedByteBuffer = ByteBuffer.wrap(bytes);
+                        Map<String, Object> mapEntry = read(nestedByteBuffer, mapEntryDescriptor);
+
+                        Object mapKey = mapEntry.get(keyField.getName());
+                        Object mapValue = mapEntry.get(valueField.getName());
+
+                        @SuppressWarnings("unchecked")
+                        Map<Object, Object> map = (Map<Object, Object>) resultMap.computeIfAbsent(fieldName, k -> new HashMap<>());
+                        map.put(mapKey, mapValue);
+                    } else {
+                        int length = (int) readVarint(byteBuffer);
+                        byte[] bytes = new byte[length];
+                        byteBuffer.get(bytes);
+                        if (fieldDescriptor.getType() == FieldDescriptor.Type.STRING) {
+                            String string = new String(bytes, StandardCharsets.UTF_8);
+                            addToResultMap(resultMap, fieldName, string, fieldDescriptor.isRepeated(), isOneof);
+                        } else if (!isOneof && fieldDescriptor.getType() == FieldDescriptor.Type.MESSAGE) {
+                            ByteBuffer nestedByteBuffer = ByteBuffer.wrap(bytes);
+                            Map<String, Object> nestedMessage = read(nestedByteBuffer, fieldDescriptor.getMessageType());
+                            addToResultMap(resultMap, fieldName, nestedMessage, fieldDescriptor.isRepeated(), isOneof);
+                        } else {
+                            addToResultMap(resultMap, fieldName, bytes, fieldDescriptor.isRepeated(), isOneof);
+                        }
+                    }
+                    break;
+                case 5: // Fixed32, SFixed32, Float
+                    int rawFixed32Value = byteBuffer.order(ByteOrder.LITTLE_ENDIAN).getInt();
+                    if (fieldDescriptor.getType() == FieldDescriptor.Type.FLOAT) {
+                        addToResultMap(resultMap, fieldName, Float.intBitsToFloat(rawFixed32Value), fieldDescriptor.isRepeated(), isOneof);
+                    } else {
+                        // Fixed32 and SFixed32 are both encoded as 32-bit integers
+                        addToResultMap(resultMap, fieldName, rawFixed32Value, fieldDescriptor.isRepeated(), isOneof);
+                    }
+                    break;
+                default:
+                    throw new UnsupportedOperationException("Unsupported wire type: " + wireType);
+            }
+        }
+
+        return resultMap;
+    }
+
+    private static void addToResultMap(Map<String, Object> resultMap, String fieldName, Object value, boolean isRepeated, boolean isOneof) {
+        if (isOneof || !isRepeated) {
+            resultMap.put(fieldName, value);
+        } else {
+            if (resultMap.containsKey(fieldName)) {
+                @SuppressWarnings("unchecked")
+                List<Object> values = (List<Object>) resultMap.get(fieldName);
+                values.add(value);
+            } else {
+                List<Object> values = new ArrayList<>();
+                values.add(value);
+                resultMap.put(fieldName, values);
+            }
+        }
+    }
+
+    private static long readVarint(ByteBuffer byteBuffer) {
+        long result = 0;
+        int shift = 0;
+        int currentByte;
+
+        do {
+            currentByte = byteBuffer.get() & 0xFF;
+            result |= (long) (currentByte & 0x7F) << shift;
+            shift += 7;
+        } while ((currentByte & 0x80) != 0);
+
+        return result;
+    }
+
+    private static int decodeZigZag32(int n) {
+        return (n >>> 1) ^ -(n & 1);
+    }
+
+    private static long decodeZigZag64(long n) {
+        return (n >>> 1) ^ -(n & 1);
+    }
+}
---
+++ twister-proto/src/test/java/dev/twister/proto/BytesToPojoTest.java
@@ -0,0 +1,239 @@
+package dev.twister.proto;
+
+import com.github.os72.protobuf.dynamic.DynamicSchema;
+
+import com.github.os72.protobuf.dynamic.EnumDefinition;
+import com.github.os72.protobuf.dynamic.MessageDefinition;
+import com.google.protobuf.Descriptors;
+import com.google.protobuf.Descriptors.Descriptor;
+import com.google.protobuf.DynamicMessage;
+
+import junit.framework.TestCase;
+
+import java.nio.ByteBuffer;
+import java.util.List;
+import java.util.Map;
+import static org.junit.Assert.*;
+
+public class BytesToPojoTest extends TestCase {
+    public void testRead() throws Exception {
+        // Load the schema from the .proto schema string
+        DynamicSchema.Builder schemaBuilder = DynamicSchema.newBuilder();
+        schemaBuilder.setName("PersonSchemaDynamic.proto");
+
+        MessageDefinition msgDef = MessageDefinition.newBuilder("TestMessage")
+                .addField("required", "int32", "int_field", 1)
+                .addField("required", "string", "str_field", 2)
+                .addField("repeated", "int32", "repeated_field", 3)
+                .build();
+
+        schemaBuilder.addMessageDefinition(msgDef);
+        DynamicSchema schema = schemaBuilder.build();
+        Descriptor descriptor = schema.getMessageDescriptor("TestMessage");
+
+        // Create a test message using the descriptor
+        DynamicMessage.Builder dynamicMessageBuilder = DynamicMessage.newBuilder(descriptor);
+        dynamicMessageBuilder.setField(descriptor.findFieldByName("int_field"), 150);
+        dynamicMessageBuilder.setField(descriptor.findFieldByName("str_field"), "foo");
+        dynamicMessageBuilder.addRepeatedField(descriptor.findFieldByName("repeated_field"), 2147483647);
+        dynamicMessageBuilder.addRepeatedField(descriptor.findFieldByName("repeated_field"), 7);
+
+        // Serialize the DynamicMessage to a ByteBuffer
+        ByteBuffer byteBuffer = ByteBuffer.wrap(dynamicMessageBuilder.build().toByteArray());
+
+        // Convert the byte buffer to a map using the descriptor
+        Map<String, Object> resultMap = BytesToPojo.read(byteBuffer, descriptor);
+
+        // Verify the result
+        assertEquals(150, resultMap.get("int_field"));
+        assertEquals("foo", resultMap.get("str_field"));
+        assertEquals(List.of(2147483647, 7), resultMap.get("repeated_field"));
+    }
+
+    public void testReadOneof() throws Exception {
+        // Load the schema from the .proto schema string
+        DynamicSchema.Builder schemaBuilder = DynamicSchema.newBuilder();
+        schemaBuilder.setName("OneofSchemaDynamic.proto");
+
+        MessageDefinition msgDef = MessageDefinition.newBuilder("OneofTestMessage")
+                .addField("optional", "int32", "optional_field", 3)
+                .addField("required", "string", "required_field", 4)
+                .addOneof("oneof_field")
+                .addField("int32", "int_field", 1)
+                .addField("string", "str_field", 2)
+                .msgDefBuilder()
+                .build();
+
+        schemaBuilder.addMessageDefinition(msgDef);
+        DynamicSchema schema = schemaBuilder.build();
+        Descriptor descriptor = schema.getMessageDescriptor("OneofTestMessage");
+
+        // Create a test message using the descriptor
+        DynamicMessage.Builder dynamicMessageBuilder = DynamicMessage.newBuilder(descriptor);
+        dynamicMessageBuilder.setField(descriptor.findFieldByName("required_field"), "bar");
+        dynamicMessageBuilder.setField(descriptor.findFieldByName("str_field"), "oneof string");
+
+        // Serialize the DynamicMessage to a ByteBuffer
+        ByteBuffer byteBuffer = ByteBuffer.wrap(dynamicMessageBuilder.build().toByteArray());
+
+        // Convert the byte buffer to a map using the descriptor
+        Map<String, Object> resultMap = BytesToPojo.read(byteBuffer, descriptor);
+
+        // Verify the result
+        assertNull(resultMap.get("int_field"));
+        assertNull(resultMap.get("optional_field"));
+        assertEquals("bar", resultMap.get("required_field"));
+        assertEquals("oneof string", resultMap.get("str_field"));
+        assertNull(resultMap.get("float_field"));
+    }
+
+    public void testReadNestedMessage() throws Exception {
+        // Load the schema from the .proto schema string
+        DynamicSchema.Builder schemaBuilder = DynamicSchema.newBuilder();
+        schemaBuilder.setName("NestedSchemaDynamic.proto");
+
+        MessageDefinition nestedMsgDef = MessageDefinition.newBuilder("NestedMessage")
+                .addField("required", "int32", "nested_int_field", 1)
+                .addField("required", "string", "nested_str_field", 2)
+                .build();
+
+        MessageDefinition msgDef = MessageDefinition.newBuilder("OuterMessage")
+                .addField("required", "int32", "outer_int_field", 1)
+                .addField("required", "NestedMessage", "nested_message", 2)
+                .build();
+
+        schemaBuilder.addMessageDefinition(nestedMsgDef);
+        schemaBuilder.addMessageDefinition(msgDef);
+        DynamicSchema schema = schemaBuilder.build();
+
+        Descriptor nestedDescriptor = schema.getMessageDescriptor("NestedMessage");
+        Descriptor outerDescriptor = schema.getMessageDescriptor("OuterMessage");
+
+        // Create a nested message using the descriptor
+        DynamicMessage.Builder nestedMessageBuilder = DynamicMessage.newBuilder(nestedDescriptor);
+        nestedMessageBuilder.setField(nestedDescriptor.findFieldByName("nested_int_field"), 42);
+        nestedMessageBuilder.setField(nestedDescriptor.findFieldByName("nested_str_field"), "nested string");
+
+        // Create an outer message using the descriptor
+        DynamicMessage.Builder outerMessageBuilder = DynamicMessage.newBuilder(outerDescriptor);
+        outerMessageBuilder.setField(outerDescriptor.findFieldByName("outer_int_field"), 100);
+        outerMessageBuilder.setField(outerDescriptor.findFieldByName("nested_message"), nestedMessageBuilder.build());
+
+        // Serialize the outer DynamicMessage to a ByteBuffer
+        ByteBuffer byteBuffer = ByteBuffer.wrap(outerMessageBuilder.build().toByteArray());
+
+        // Convert the byte buffer to a map using the descriptor
+        Map<String, Object> resultMap = BytesToPojo.read(byteBuffer, outerDescriptor);
+
+        // Verify the result
+        assertEquals(100, resultMap.get("outer_int_field"));
+        Map<String, Object> nestedResultMap = (Map<String, Object>) resultMap.get("nested_message");
+        assertNotNull(nestedResultMap);
+        assertEquals(42, nestedResultMap.get("nested_int_field"));
+        assertEquals("nested string", nestedResultMap.get("nested_str_field"));
+    }
+
+    public void testPrimitiveTypes() throws Exception {
+        // Load the schema from the .proto schema string
+        DynamicSchema.Builder schemaBuilder = DynamicSchema.newBuilder();
+        schemaBuilder.setName("PrimitiveSchemaDynamic.proto");
+
+        MessageDefinition msgDef = MessageDefinition.newBuilder("PrimitiveMessage")
+                .addField("optional", "int32", "int32_field", 1)
+                .addField("optional", "int64", "int64_field", 2)
+                .addField("optional", "uint32", "uint32_field", 3)
+                .addField("optional", "uint64", "uint64_field", 4)
+                .addField("optional", "sint32", "sint32_field", 5)
+                .addField("optional", "sint64", "sint64_field", 6)
+                .addField("optional", "bool", "bool_field", 7)
+                .addField("optional", "fixed64", "fixed64_field", 8)
+                .addField("optional", "sfixed64", "sfixed64_field", 9)
+                .addField("optional", "double", "double_field", 10)
+                .addField("optional", "fixed32", "fixed32_field", 11)
+                .addField("optional", "sfixed32", "sfixed32_field", 12)
+                .addField("optional", "float", "float_field", 13)
+                .addField("optional", "bytes", "bytes_field", 14)
+                .build();
+
+        schemaBuilder.addMessageDefinition(msgDef);
+        DynamicSchema schema = schemaBuilder.build();
+        Descriptor descriptor = schema.getMessageDescriptor("PrimitiveMessage");
+
+        // Create a test message using the descriptor
+        DynamicMessage.Builder dynamicMessageBuilder = DynamicMessage.newBuilder(descriptor);
+        dynamicMessageBuilder.setField(descriptor.findFieldByName("int32_field"), 123);
+        dynamicMessageBuilder.setField(descriptor.findFieldByName("int64_field"), 1234567890123456789L);
+        dynamicMessageBuilder.setField(descriptor.findFieldByName("uint32_field"), 456);
+        dynamicMessageBuilder.setField(descriptor.findFieldByName("uint64_field"), Long.parseUnsignedLong("9876543210987654321"));
+        dynamicMessageBuilder.setField(descriptor.findFieldByName("sint32_field"), -789);
+        dynamicMessageBuilder.setField(descriptor.findFieldByName("sint64_field"), -1234567890987654321L);
+        dynamicMessageBuilder.setField(descriptor.findFieldByName("bool_field"), true);
+        dynamicMessageBuilder.setField(descriptor.findFieldByName("fixed64_field"), 1234567890L);
+        dynamicMessageBuilder.setField(descriptor.findFieldByName("sfixed64_field"), -1234567890L);
+        dynamicMessageBuilder.setField(descriptor.findFieldByName("double_field"), 1234.5678);
+        dynamicMessageBuilder.setField(descriptor.findFieldByName("fixed32_field"), 12345678);
+        dynamicMessageBuilder.setField(descriptor.findFieldByName("sfixed32_field"), -12345678);
+        dynamicMessageBuilder.setField(descriptor.findFieldByName("float_field"), 12.34f);
+        dynamicMessageBuilder.setField(descriptor.findFieldByName("bytes_field"), new byte[]{1, 2, 3, 4, 5});
+
+        // Serialize the DynamicMessage to a ByteBuffer
+        ByteBuffer byteBuffer = ByteBuffer.wrap(dynamicMessageBuilder.build().toByteArray());
+
+        // Convert the byte buffer to a map using the descriptor
+        Map<String, Object> resultMap = BytesToPojo.read(byteBuffer, descriptor);
+
+        // Verify the result
+        assertEquals(123, resultMap.get("int32_field"));
+        assertEquals(1234567890123456789L, resultMap.get("int64_field"));
+        assertEquals(456L, resultMap.get("uint32_field"));
+        assertEquals(Long.parseUnsignedLong("9876543210987654321"), resultMap.get("uint64_field"));
+        assertEquals(-789, resultMap.get("sint32_field"));
+        assertEquals(-1234567890987654321L, resultMap.get("sint64_field"));
+        assertEquals(true, resultMap.get("bool_field"));
+        assertEquals(1234567890L, resultMap.get("fixed64_field"));
+        assertEquals(-1234567890L, resultMap.get("sfixed64_field"));
+        assertEquals(1234.5678, resultMap.get("double_field"));
+        assertEquals(12345678, resultMap.get("fixed32_field"));
+        assertEquals(-12345678, resultMap.get("sfixed32_field"));
+        assertEquals(12.34f, resultMap.get("float_field"));
+        assertArrayEquals(new byte[]{1, 2, 3, 4, 5}, (byte[]) resultMap.get("bytes_field"));
+    }
+
+    public void testEnumType() throws Exception {
+        // Define an enum type
+        EnumDefinition enumDefinition = EnumDefinition.newBuilder("TestEnum")
+                .addValue("ENUM_VALUE_1", 1)
+                .addValue("ENUM_VALUE_2", 2)
+                .build();
+
+        // Load the schema from the .proto schema string
+        DynamicSchema.Builder schemaBuilder = DynamicSchema.newBuilder();
+        schemaBuilder.setName("EnumSchemaDynamic.proto");
+
+        schemaBuilder.addEnumDefinition(enumDefinition);
+
+        MessageDefinition msgDef = MessageDefinition.newBuilder("EnumMessage")
+                .addField("optional", "TestEnum", "enum_field", 1)
+                .build();
+
+        schemaBuilder.addMessageDefinition(msgDef);
+        DynamicSchema schema = schemaBuilder.build();
+
+        Descriptor descriptor = schema.getMessageDescriptor("EnumMessage");
+        Descriptors.EnumDescriptor enumDescriptor = schema.getEnumDescriptor("TestEnum");
+
+        // Create a test message using the descriptor
+        DynamicMessage.Builder dynamicMessageBuilder = DynamicMessage.newBuilder(descriptor);
+        Descriptors.EnumValueDescriptor enumValueDescriptor = enumDescriptor.findValueByName("ENUM_VALUE_2");
+        dynamicMessageBuilder.setField(descriptor.findFieldByName("enum_field"), enumValueDescriptor);
+
+        // Serialize the DynamicMessage to a ByteBuffer
+        ByteBuffer byteBuffer = ByteBuffer.wrap(dynamicMessageBuilder.build().toByteArray());
+
+        // Convert the byte buffer to a map using the descriptor
+        Map<String, Object> resultMap = BytesToPojo.read(byteBuffer, descriptor);
+
+        // Verify the result
+        assertEquals("ENUM_VALUE_2", resultMap.get("enum_field"));
+    }
+}
