025dd1a3de34523c575fa7002e8edd546c4d91d7
Chris Riccomini <criccomini@apache.org>
2023-05-15 09:02:40 -0700

    Add AvroSchemaInferrer and Enhancements to AvroWriter
    - Introduced a new class, `AvroSchemaInferrer`, in the `twister-avro` module
      to infer Avro schemas from input objects. It supports inferring schemas
      for primitive types, arrays, maps, unions, and nested records. Added unit
      tests to verify the correctness of schema inference.
    
    - Updated the `AvroWriter` in the `twister-avro` module to utilize the new
      `AvroSchemaInferrer` for schema inference. The `writeAvro` method now
      includes an overloaded version that accepts an input object and a record
      name. This method infers the schema using `AvroSchemaInferrer` and writes
      the Avro-encoded data accordingly. Added corresponding unit tests to verify
      the writing of Avro data with inferred schemas.

---
+++ twister-avro/src/test/java/dev/twister/avro/AvroSchemaInferrerTest.java
@@ -0,0 +1,76 @@
+package dev.twister.avro;
+
+import junit.framework.TestCase;
+import org.apache.avro.Schema;
+import org.apache.avro.SchemaBuilder;
+
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+
+public class AvroSchemaInferrerTest extends TestCase {
+    public void testSchemaInferrer() {
+        Map<String, Object> testMap = new HashMap<>();
+        testMap.put("stringField", "Hello, World!");
+        testMap.put("intField", 42);
+        testMap.put("booleanField", true);
+        testMap.put("longField", 123456789L);
+        testMap.put("floatField", 3.14f);
+        testMap.put("doubleField", 2.718281828);
+        testMap.put("nullField", null);
+
+        Schema inferredSchema = new AvroSchemaInferrer().schema(testMap, "TestSchema");
+
+        // Define expected schema with fields added in alphabetical order
+        Schema expectedSchema = SchemaBuilder.record("TestSchema").fields()
+                .name("booleanField").type(SchemaBuilder.unionOf().nullType().and().booleanType().endUnion()).noDefault()
+                .name("doubleField").type(SchemaBuilder.unionOf().nullType().and().doubleType().endUnion()).noDefault()
+                .name("floatField").type(SchemaBuilder.unionOf().nullType().and().floatType().endUnion()).noDefault()
+                .name("intField").type(SchemaBuilder.unionOf().nullType().and().intType().endUnion()).noDefault()
+                .name("longField").type(SchemaBuilder.unionOf().nullType().and().longType().endUnion()).noDefault()
+                .name("nullField").type().nullType().noDefault()
+                .name("stringField").type(SchemaBuilder.unionOf().nullType().and().stringType().endUnion()).noDefault()
+                .endRecord();
+
+        // Compare inferred schema with expected schema
+        assertEquals(expectedSchema, inferredSchema);
+    }
+
+    public void testArray() {
+        AvroSchemaInferrer inferrer = new AvroSchemaInferrer();
+        Map<String, Object> map = new HashMap<>();
+        map.put("array", Arrays.asList(1, 2, 3));
+        Schema schema = inferrer.schema(map, "TestArray");
+        String expectedSchema = "{\"type\":\"record\",\"name\":\"TestArray\",\"fields\":[{\"name\":\"array\",\"type\":[\"null\",{\"type\":\"array\",\"items\":[\"null\",\"int\"]}]}]}";
+        assertEquals(expectedSchema, schema.toString());
+    }
+
+    public void testNestedRecord() {
+        AvroSchemaInferrer inferrer = new AvroSchemaInferrer();
+        Map<String, Object> map = new HashMap<>();
+        Map<String, Object> subMap = new HashMap<>();
+        subMap.put("subField", "subValue");
+        map.put("field", subMap);
+        Schema schema = inferrer.schema(map, "TestComplexMap");
+        String expectedSchema = "{\"type\":\"record\",\"name\":\"TestComplexMap\",\"fields\":[{\"name\":\"field\",\"type\":[\"null\",{\"type\":\"record\",\"name\":\"TestComplexMap_field\",\"fields\":[{\"name\":\"subField\",\"type\":[\"null\",\"string\"]}]}]}]}";
+        assertEquals(expectedSchema, schema.toString());
+    }
+
+    public void testSchemaWithMapAsMap() {
+        // Create a test map with multiple different types of values
+        Map<String, Object> map = new HashMap<>();
+        map.put("field1", "string");
+        map.put("field2", 123);
+        map.put("field3", 45.67);
+
+        // Create an AvroSchemaInferrer with mapAsRecord = false
+        AvroSchemaInferrer inferrer = new AvroSchemaInferrer(false);
+
+        // Infer the Avro schema for the map
+        Schema schema = inferrer.schema(map, "TestRecord");
+
+        // Check that the resulting schema is a map schema with a union value type
+        assertEquals(Schema.Type.MAP, schema.getType());
+        assertEquals(Schema.Type.UNION, schema.getValueType().getType());
+    }
+}
--- twister-avro/src/test/java/dev/twister/avro/AvroWriterTest.java
+++ twister-avro/src/test/java/dev/twister/avro/AvroWriterTest.java
@@ -2,15 +2,19 @@ package dev.twister.avro;
 
 import junit.framework.TestCase;
 import org.apache.avro.Schema;
+import org.apache.avro.SchemaBuilder;
 import org.apache.avro.generic.GenericDatumReader;
 import org.apache.avro.generic.GenericRecord;
+import org.apache.avro.io.DatumReader;
 import org.apache.avro.io.Decoder;
 import org.apache.avro.io.DecoderFactory;
+import org.apache.avro.specific.SpecificDatumReader;
+import org.apache.avro.util.Utf8;
 
+import java.io.ByteArrayInputStream;
 import java.nio.ByteBuffer;
-import java.util.Arrays;
-import java.util.HashMap;
-import java.util.Map;
+import java.util.*;
+import java.util.stream.Collectors;
 
 public class AvroWriterTest extends TestCase {
     public void testPrimitives() throws Exception {
@@ -93,20 +97,12 @@ public class AvroWriterTest extends TestCase {
         assertEquals(recordMap, resultMap);
     }
 
-    public void testComplexTypes() throws Exception {
-        String schemaJson = "{\"type\":\"record\",\"name\":\"TestComplexTypes\",\"fields\":[{\"name\":\"enumField\",\"type\":{\"type\":\"enum\",\"name\":\"TestEnum\",\"symbols\":[\"RED\",\"GREEN\",\"BLUE\"]}},{\"name\":\"arrayField\",\"type\":{\"type\":\"array\",\"items\":\"string\"}},{\"name\":\"mapField\",\"type\":{\"type\":\"map\",\"values\":\"int\"}},{\"name\":\"unionField\",\"type\":[\"null\",\"string\"]},{\"name\":\"fixedField\",\"type\":{\"type\":\"fixed\",\"name\":\"TestFixed\",\"size\":4}}]}";
+    public void testEnumField() throws Exception {
+        String schemaJson = "{\"type\":\"record\",\"name\":\"TestEnumRecord\",\"fields\":[{\"name\":\"enumField\",\"type\":{\"type\":\"enum\",\"name\":\"TestEnum\",\"symbols\":[\"RED\",\"GREEN\",\"BLUE\"]}}]}";
         Schema schema = new Schema.Parser().parse(schemaJson);
 
         Map<String, Object> recordMap = new HashMap<>();
         recordMap.put("enumField", "GREEN");
-        recordMap.put("arrayField", Arrays.asList("Aa", "Bb", "Cc"));
-        Map<String, Integer> map = new HashMap<>();
-        map.put("one", 1);
-        map.put("two", 2);
-        map.put("three", 3);
-        recordMap.put("mapField", map);
-        recordMap.put("unionField", "example");
-        recordMap.put("fixedField", ByteBuffer.wrap(new byte[]{1, 2, 3, 4}));
 
         ByteBuffer byteBuffer = new AvroWriter().writeAvro(recordMap, schema);
 
@@ -115,14 +111,63 @@ public class AvroWriterTest extends TestCase {
         GenericRecord genericRecord = datumReader.read(null, decoder);
 
         assertEquals("GREEN", genericRecord.get("enumField").toString());
-        assertEquals(Arrays.asList("Aa", "Bb", "Cc"), genericRecord.get("arrayField"));
-        assertEquals(map, genericRecord.get("mapField"));
+    }
+
+    public void testArrayField() throws Exception {
+        String schemaJson = "{\"type\":\"record\",\"name\":\"TestArrayRecord\",\"fields\":[{\"name\":\"arrayField\",\"type\":{\"type\":\"array\",\"items\":\"string\"}}]}";
+        Schema schema = new Schema.Parser().parse(schemaJson);
+
+        Map<String, Object> recordMap = new HashMap<>();
+        recordMap.put("arrayField", Arrays.asList("Aa", "Bb", "Cc"));
+
+        ByteBuffer byteBuffer = new AvroWriter().writeAvro(recordMap, schema);
+
+        GenericDatumReader<GenericRecord> datumReader = new GenericDatumReader<>(schema);
+        Decoder decoder = DecoderFactory.get().binaryDecoder(byteBuffer.array(), null);
+        GenericRecord genericRecord = datumReader.read(null, decoder);
+
+        List<CharSequence> utf8List = (List<CharSequence>) genericRecord.get("arrayField");
+        List<String> stringList = utf8List.stream().map(CharSequence::toString).collect(Collectors.toList());
+        assertEquals(Arrays.asList("Aa", "Bb", "Cc"), stringList);
+    }
+
+    public void testMapField() throws Exception {
+        String schemaJson = "{\"type\":\"record\",\"name\":\"TestMapRecord\",\"fields\":[{\"name\":\"mapField\",\"type\":{\"type\":\"map\",\"values\":\"int\"}}]}";
+        Schema schema = new Schema.Parser().parse(schemaJson);
+
+        Map<String, Object> recordMap = new HashMap<>();
+        Map<String, Integer> map = new TreeMap<>();
+        map.put("one", 1);
+        map.put("two", 2);
+        map.put("three", 3);
+        recordMap.put("mapField", map);
+
+        ByteBuffer byteBuffer = new AvroWriter().writeAvro(recordMap, schema);
+
+        GenericDatumReader<GenericRecord> datumReader = new GenericDatumReader<>(schema);
+        Decoder decoder = DecoderFactory.get().binaryDecoder(byteBuffer.array(), null);
+        GenericRecord genericRecord = datumReader.read(null, decoder);
+
+        Map<Object, Object> actualMap = (Map<Object, Object>) genericRecord.get("mapField");
+        assertEquals(1, actualMap.get(new Utf8("one")));
+        assertEquals(2, actualMap.get(new Utf8("two")));
+        assertEquals(3, actualMap.get(new Utf8("three")));
+    }
+
+    public void testUnionField() throws Exception {
+        String schemaJson = "{\"type\":\"record\",\"name\":\"TestUnionRecord\",\"fields\":[{\"name\":\"unionField\",\"type\":[\"null\",\"string\"]}]}";
+        Schema schema = new Schema.Parser().parse(schemaJson);
+
+        Map<String, Object> recordMap = new HashMap<>();
+        recordMap.put("unionField", "example");
+
+        ByteBuffer byteBuffer = new AvroWriter().writeAvro(recordMap, schema);
+
+        GenericDatumReader<GenericRecord> datumReader = new GenericDatumReader<>(schema);
+        Decoder decoder = DecoderFactory.get().binaryDecoder(byteBuffer.array(), null);
+        GenericRecord genericRecord = datumReader.read(null, decoder);
+
         assertEquals("example", genericRecord.get("unionField").toString());
-        ByteBuffer decodedBytes = (ByteBuffer) genericRecord.get("fixedField");
-        assertEquals(4, decodedBytes.remaining());
-        for (int i = 1; i <= 4; i++) {
-            assertEquals(i, decodedBytes.get());
-        }
     }
 
     public void testBidirectionalComplexTypes() throws Exception {
@@ -152,5 +197,45 @@ public class AvroWriterTest extends TestCase {
         for (int i = 1; i <= 4; i++) {
             assertEquals(i, decodedBytes.get());
         }
+    }
+
+    public void testWriteWithInferredSchema() throws Exception {
+        // Define a map of values
+        Map<String, Object> testMap = new HashMap<>();
+        testMap.put("stringField", "Hello, World!");
+        testMap.put("intField", 42);
+        testMap.put("booleanField", true);
+        testMap.put("longField", 123456789L);
+        testMap.put("floatField", 3.14f);
+        testMap.put("doubleField", 2.718281828);
+        testMap.put("nullField", null);
+
+        // Write the map to Avro
+        AvroWriter avroWriter = new AvroWriter();
+        ByteBuffer avroData = avroWriter.writeAvro(testMap, "TestSchema");
+
+        // Manually create expected schema
+        Schema expectedSchema = SchemaBuilder.record("TestSchema").fields()
+                .name("booleanField").type().nullable().booleanType().noDefault()
+                .name("doubleField").type().nullable().doubleType().noDefault()
+                .name("floatField").type().nullable().floatType().noDefault()
+                .name("intField").type().nullable().intType().noDefault()
+                .name("longField").type().nullable().longType().noDefault()
+                .name("nullField").type().nullType().noDefault()
+                .name("stringField").type().nullable().stringType().noDefault()
+                .endRecord();
+
+        DatumReader<GenericRecord> reader = new SpecificDatumReader<>(expectedSchema);
+        Decoder decoder = DecoderFactory.get().binaryDecoder(new ByteArrayInputStream(avroData.array()), null);
+        GenericRecord record = reader.read(null, decoder);
+
+        // Validate the read back data
+        assertEquals("Hello, World!", record.get("stringField").toString());
+        assertEquals(42, record.get("intField"));
+        assertEquals(true, record.get("booleanField"));
+        assertEquals(123456789L, record.get("longField"));
+        assertEquals(3.14f, (float)record.get("floatField"), 0.001);
+        assertEquals(2.718281828, (double)record.get("doubleField"), 0.001);
+        assertEquals(null, record.get("nullField"));
     }
 }
