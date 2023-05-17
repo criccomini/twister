ec0fde9928fc27185988aefce2c32d2bb82b4eaf
Chris Riccomini <criccomini@apache.org>
2023-05-16 17:08:02 -0700

    Add support for repeated fields in ProtoDescriptorInferrer
    The ProtoDescriptorInferrer class now supports map values that are
    List objects, treating them as repeated fields in the generated Protocol
    Buffers descriptor. The inferFieldType method has been updated to
    detect List values and set the field label as LABEL_REPEATED. The
    testInferRepeatedFieldType test case has been added to verify the
    behavior.

--- twister-proto/src/test/java/dev/twister/proto/ProtoDescriptorInferrerTest.java
+++ twister-proto/src/test/java/dev/twister/proto/ProtoDescriptorInferrerTest.java
@@ -7,14 +7,14 @@ import junit.framework.TestCase;
 
 import java.math.BigInteger;
 import java.nio.ByteBuffer;
-import java.util.HashMap;
+import java.util.Arrays;
 import java.util.LinkedHashMap;
+import java.util.List;
 import java.util.Map;
 
 public class ProtoDescriptorInferrerTest extends TestCase {
 
-    public void testInferFieldType() throws Exception {
-        ProtoDescriptorInferrer inferrer = new ProtoDescriptorInferrer();
+    public void testPrimitives() throws Exception {
         Map<String, Object> object = new LinkedHashMap<>();
         object.put("fieldInt", Integer.valueOf(32));
         object.put("fieldLong", Long.valueOf(64));
@@ -25,7 +25,7 @@ public class ProtoDescriptorInferrerTest extends TestCase {
         object.put("fieldFloat", Float.valueOf(32.0f));
         object.put("fieldBigInt", BigInteger.valueOf(Long.MAX_VALUE));
 
-        Descriptors.Descriptor descriptor = inferrer.descriptor(object, "TestMessage");
+        Descriptors.Descriptor descriptor = new ProtoDescriptorInferrer().descriptor(object, "TestMessage");
 
         // Create expected schema using DynamicSchema
         DynamicSchema.Builder schemaBuilder = DynamicSchema.newBuilder();
@@ -49,4 +49,28 @@ public class ProtoDescriptorInferrerTest extends TestCase {
         // Compare the descriptors
         assertEquals(expectedDescriptor.toProto(), descriptor.toProto());
     }
+
+    public void testInferRepeatedFieldType() throws Exception {
+        Map<String, Object> object = new LinkedHashMap<>();
+        List<Integer> intList = Arrays.asList(32, 33, 34);
+        object.put("fieldRepeated", intList);
+
+        Descriptors.Descriptor descriptor = new ProtoDescriptorInferrer().descriptor(object, "TestMessage");
+
+        // Create expected schema using DynamicSchema
+        DynamicSchema.Builder schemaBuilder = DynamicSchema.newBuilder();
+        schemaBuilder.setName("TestMessage.proto");
+
+        MessageDefinition msgDef = MessageDefinition.newBuilder("TestMessage")
+                .addField("repeated", "int32", "fieldRepeated", 1)
+                .build();
+
+        schemaBuilder.addMessageDefinition(msgDef);
+        DynamicSchema expectedSchema = schemaBuilder.build();
+        Descriptors.Descriptor expectedDescriptor = expectedSchema.getMessageDescriptor("TestMessage");
+
+        // Compare the descriptors
+        assertEquals(expectedDescriptor.toProto(), descriptor.toProto());
+    }
+
 }
