ad55c7c20372381c4eff5f142ee254199dbcebad
Chris Riccomini <criccomini@apache.org>
2023-05-16 17:08:02 -0700

    Add support for repeated fields in ProtoDescriptorInferrer
    The ProtoDescriptorInferrer class now supports map values that are
    List objects, treating them as repeated fields in the generated Protocol
    Buffers descriptor. The inferFieldType method has been updated to
    detect List values and set the field label as LABEL_REPEATED. The
    testInferRepeatedFieldType test case has been added to verify the
    behavior.

--- twister-proto/src/main/java/dev/twister/proto/ProtoDescriptorInferrer.java
+++ twister-proto/src/main/java/dev/twister/proto/ProtoDescriptorInferrer.java
@@ -5,28 +5,38 @@ import com.google.protobuf.Descriptors;
 
 import java.math.BigInteger;
 import java.nio.ByteBuffer;
+import java.util.List;
 import java.util.Map;
 
 public class ProtoDescriptorInferrer {
     public Descriptors.Descriptor descriptor(Map<String, Object> object, String messageName) {
         DescriptorProtos.FileDescriptorProto.Builder fileDescriptorBuilder = DescriptorProtos.FileDescriptorProto.newBuilder();
         DescriptorProtos.DescriptorProto.Builder messageBuilder = DescriptorProtos.DescriptorProto.newBuilder();
-        DescriptorProtos.FieldDescriptorProto.Builder fieldBuilder = DescriptorProtos.FieldDescriptorProto.newBuilder();
         int fieldNumber = 1;
 
         // Set message name
         messageBuilder.setName(messageName);
 
         for (Map.Entry<String, Object> entry : object.entrySet()) {
+            Descriptors.FieldDescriptor.Type fieldType;
             String fieldName = entry.getKey();
             Object fieldValue = entry.getValue();
-            Descriptors.FieldDescriptor.Type fieldType = inferFieldType(fieldValue);
+            DescriptorProtos.FieldDescriptorProto.Builder fieldBuilder = DescriptorProtos.FieldDescriptorProto.newBuilder();
 
             // Set field name and type
             fieldBuilder.setName(fieldName);
             fieldBuilder.setNumber(fieldNumber++);
+
+            if (fieldValue instanceof List) {
+                fieldBuilder.setLabel(DescriptorProtos.FieldDescriptorProto.Label.LABEL_REPEATED);
+                // assuming all elements in the list are of the same type
+                fieldType = inferFieldType(((List<?>) fieldValue).get(0));
+            } else {
+                fieldBuilder.setLabel(DescriptorProtos.FieldDescriptorProto.Label.LABEL_OPTIONAL);
+                fieldType = inferFieldType(fieldValue);
+            }
+
             fieldBuilder.setType(fieldType.toProto());
-            fieldBuilder.setLabel(DescriptorProtos.FieldDescriptorProto.Label.LABEL_OPTIONAL);
 
             // Add field to the message
             messageBuilder.addField(fieldBuilder.build());
